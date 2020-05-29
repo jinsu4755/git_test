d6716a4e7428c196b03bf34a7ecb892b799b2803
jinsu4755 <parkjinsu4755@gmail.com>
2020-05-29 23:44:38 +0900

    nutee Kotlin

---
+++ app/src/main/java/com/antilog/nuteekotlin/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.antilog.nuteekotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    //lateinit var 은 여기
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //변수는 여기
+
+    }
+
+    //함수는 여기
+}
